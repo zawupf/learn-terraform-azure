0e64ad2e6f6a00f4b99566166da9fc4db3d379dd
Arnold Filip <arnold.filip@technia.com>
2021-02-02 08:55:55 +0100

    Use variables

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# Configure the Microsoft Azure Provider.
 terraform {
   required_providers {
     azurerm = {
@@ -13,36 +14,23 @@ provider "azurerm" {
 
 # Create a resource group
 resource "azurerm_resource_group" "rg" {
-  name     = "myTFResourceGroup"
-  location = "West Europe"
-
-  tags = {
-    Environment = "Terraform Getting Started"
-    Team        = "DevOps"
-  }
+  name     = "${var.prefix}TFRG"
+  location = var.location
+  tags     = var.tags
 }
 
-# Create a virtual network
+# Create virtual network
 resource "azurerm_virtual_network" "vnet" {
-  name                = "myTFVnet"
+  name                = "${var.prefix}TFVnet"
   address_space       = ["10.0.0.0/16"]
-  location            = "West Europe"
+  location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
-}
-
-variable "admin_username" {
-  type        = string
-  description = "Administrator user name for virtual machine"
-}
-
-variable "admin_password" {
-  type        = string
-  description = "Password must meet Azure complexity requirements"
+  tags                = var.tags
 }
 
 # Create subnet
 resource "azurerm_subnet" "subnet" {
-  name                 = "myTFSubnet"
+  name                 = "${var.prefix}TFSubnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
@@ -50,18 +38,19 @@ resource "azurerm_subnet" "subnet" {
 
 # Create public IP
 resource "azurerm_public_ip" "publicip" {
-  name                = "myTFPublicIP"
-  location            = "West Europe"
+  name                = "${var.prefix}TFPublicIP"
+  location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
+  allocation_method   = "Dynamic"
+  tags                = var.tags
 }
-
 
 # Create Network Security Group and rule
 resource "azurerm_network_security_group" "nsg" {
-  name                = "myTFNSG"
-  location            = "West Europe"
+  name                = "${var.prefix}TFNSG"
+  location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
+  tags                = var.tags
 
   security_rule {
     name                       = "SSH"
@@ -78,12 +67,13 @@ resource "azurerm_network_security_group" "nsg" {
 
 # Create network interface
 resource "azurerm_network_interface" "nic" {
-  name                = "myNIC"
-  location            = "West Europe"
+  name                = "${var.prefix}NIC"
+  location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
+  tags                = var.tags
 
   ip_configuration {
-    name                          = "myNICConfg"
+    name                          = "${var.prefix}NICConfg"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "dynamic"
     public_ip_address_id          = azurerm_public_ip.publicip.id
@@ -92,14 +82,15 @@ resource "azurerm_network_interface" "nic" {
 
 # Create a Linux virtual machine
 resource "azurerm_virtual_machine" "vm" {
-  name                  = "myTFVM"
-  location              = "West Europe"
+  name                  = "${var.prefix}TFVM"
+  location              = var.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.nic.id]
   vm_size               = "Standard_DS1_v2"
+  tags                  = var.tags
 
   storage_os_disk {
-    name              = "myOsDisk"
+    name              = "${var.prefix}OsDisk"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Premium_LRS"
@@ -108,13 +99,12 @@ resource "azurerm_virtual_machine" "vm" {
   storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "16.04.0-LTS"
-    # sku       = "18.04-LTS"
-    version = "latest"
+    sku       = lookup(var.sku, var.location)
+    version   = "latest"
   }
 
   os_profile {
-    computer_name  = "myTFVM"
+    computer_name  = "${var.prefix}TFVM"
     admin_username = var.admin_username
     admin_password = var.admin_password
   }
@@ -122,10 +112,15 @@ resource "azurerm_virtual_machine" "vm" {
   os_profile_linux_config {
     disable_password_authentication = false
   }
+
 }
 
 data "azurerm_public_ip" "ip" {
   name                = azurerm_public_ip.publicip.name
   resource_group_name = azurerm_virtual_machine.vm.resource_group_name
   depends_on          = [azurerm_virtual_machine.vm]
+}
+
+output "os_sku" {
+  value = lookup(var.sku, var.location)
 }
---
+++ variables.tf
@@ -0,0 +1,33 @@
+variable "location" {}
+
+variable "admin_username" {
+  type        = string
+  description = "Administrator user name for virtual machine"
+}
+
+variable "admin_password" {
+  type        = string
+  description = "Password must meet Azure complexity requirements"
+}
+
+variable "prefix" {
+  type    = string
+  default = "my"
+}
+
+variable "tags" {
+  type = map(any)
+
+  default = {
+    Environment = "Terraform GS"
+    Dept        = "Engineering"
+  }
+}
+
+variable "sku" {
+  default = {
+    westus2    = "16.04-LTS"
+    eastus     = "18.04-LTS"
+    westeurope = "18.04-LTS"
+  }
+}
